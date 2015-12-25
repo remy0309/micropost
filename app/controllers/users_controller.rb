da62a96ef71f8a07d19d6411a1124e25370b940d
remy0309 <stereal.0309@gmail.com>
2015-12-25 05:36:12 +0000

    nothing changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,6 @@ class UsersController < ApplicationController
   def show # 追加
    @user = User.find(params[:id])
   end
-  
-  def new
-    @user = User.new
-  end
 
   def create
     @user = User.new(user_params)
