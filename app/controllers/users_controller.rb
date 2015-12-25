4c86a0bd14bf1eef4402dffa73be5b290cc0ddbb
remy0309 <stereal.0309@gmail.com>
2015-12-25 05:31:35 +0000

    nothing special changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,6 @@ class UsersController < ApplicationController
   def show # 追加
    @user = User.find(params[:id])
   end
-  
-  def new
-    @user = User.new
-  end
 
   def create
     @user = User.new(user_params)
@@ -21,10 +17,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "update succeed"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :profile)
   end
 end
